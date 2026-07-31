9d9581d4050f66a5ad85bfbf198c21976144aa3a
Saleem Ansari <tuxdna@gmail.com>
2026-08-01 02:08:35 +0530

    first script from lesson

---
+++ bash_scripts/first.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+echo "What is your Name:"
+read name
+
+echo "Hello $name, welcome to the world of Bash Scripting!"
+echo "Today is $(date)"
+echo "Your current workign directory is: $(pwd)"
+
