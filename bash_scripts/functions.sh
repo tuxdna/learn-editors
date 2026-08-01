0f501f56e6f7e416c9aff9ea2b39590dcca5b137
Saleem Ansari <tuxdna@gmail.com>
2026-08-01 16:06:07 +0530

    more about functions and linux commands

--- bash_scripts/functions.sh
+++ bash_scripts/functions.sh
@@ -1,11 +1,58 @@
 #!/usr/bin/env bash
 
-function great {
+function greet {
 	local name="Bob"
 	echo "Hello $name!"
 }
 
-great
+greet
 
 echo "Outside the function, name=$name"
+
+
+name=User
+say_hello() {
+	echo "Hello $name!"
+	echo "Argument1: $1!"
+}
+
+echo Lets call the function
+say_hello
+say_hello John
+
+# A function returns value in three ways
+# 1 - stdout
+# 2 - using named refence to passed variables and setting value to those variables
+# 3 - exit code using return statement, this is checked using $? after function call
+#
+
+## 1 - stdout example
+
+add_numbers_v1() {
+	echo $(( $1 + $2 ))
+}
+echo "V1: The sum is $(add_numbers_v1 4 5)"
+
+## 2 - named ref to arguments
+add_numbers_v2() {
+	local n1=$1
+	local n2=$2
+	local -n output=$3
+	output=$(( $n1 + $n2 ))
+}
+add_numbers_v2 9 5 result
+echo "V2: The sum is $result"
+
+## 3 - return exit code
+check_file() {
+	if [ -f "$1" ]; then
+		return 0
+	else
+		return 1
+	fi
+}
+echo "Checking if config.txt exists"
+check_file "config.txt"
+status=$?
+echo "Exit status was $status"
 
--- bash_scripts/other_commands.sh
+++ bash_scripts/other_commands.sh
@@ -10,3 +10,12 @@ rm
 rmdir
 
 chmod  # u g o  + - r w x
+
+uptime
+cat /proc/loadavg
+# search for files larger than given size
+find "$search_dir" -type -f -size +"${min_size}"M -exec  ls -lh \; | sort -k5 -rh
+df -h
+free -h
+ps aux --sort=-%cpu | head -n 6
+
